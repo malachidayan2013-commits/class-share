a982f99f7b8bfa8235dffae769d3f35dbf46b0bf
Malachi Dayan <malachidayan2013@gmail.com>
2026-02-14 19:30:19 +0200

    yyy

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 if not os.path.exists(DATA_FILE):
-    with open(DATA_FILE, "w") as f:
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
         json.dump([], f)
 
 
 def load_data():
-    with open(DATA_FILE, "r") as f:
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
 def save_data(data):
-    with open(DATA_FILE, "w") as f:
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
 
 
